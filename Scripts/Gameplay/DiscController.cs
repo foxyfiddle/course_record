996cbb5047c6b022d878f74459e001ec6ece7de5
Josh B <foxandthefiddle@comcast.net>
2026-03-31 18:38:03 -0500

    feat: add disc animation frames

--- Scripts/Gameplay/DiscController.cs
+++ Scripts/Gameplay/DiscController.cs
@@ -3,17 +3,20 @@ using System;
 
 public partial class DiscController : CharacterBody2D
 {
+	// variables for disc movement
 	private Vector2 direction = Vector2.Zero;
 	private float Speed = 0f;
 	private float remainingDistance = 0f;
+	private float totalDistance = 0f;
 	private bool isLaunched = false;
 
-	public void Launch(Vector2 launchDirection, float launchDistance, float launchSpeed)
+	// variables for disc animation
+	private AnimatedSprite2D _animatedSprite;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
-		direction = launchDirection.Normalized();
-		remainingDistance = launchDistance;
-		Speed = launchSpeed;
-		isLaunched = true;
+		_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,6 +27,7 @@ public partial class DiscController : CharacterBody2D
 			return;
 
 		float frameDistance = Speed * (float)delta;
+		float flightLeftRatio = remainingDistance / totalDistance;
 
 		// If this frame would overshoot, clamp to the remaining distance
 		if (frameDistance > remainingDistance)
@@ -39,8 +43,27 @@ public partial class DiscController : CharacterBody2D
 		}
 
 		Velocity = direction * Speed;
+
+		_animatedSprite.SpeedScale = flightLeftRatio; // Slow down animation as it approaches the target
 		MoveAndSlide();
 
 		remainingDistance -= frameDistance;
+	}
+
+	public void Launch(Vector2 launchDirection, float launchDistance, float launchSpeed)
+	{
+		direction = launchDirection.Normalized();
+		remainingDistance = launchDistance;
+		totalDistance  = launchDistance;
+		Speed = launchSpeed;
+		isLaunched = true;
+
+		StartSpin();
+	}
+
+	// animation logic
+	public void StartSpin()
+	{
+		_animatedSprite.Play("spin");
 	}
 }
